057e4b2d95b561579a9d5e8eef522f009b342562
Karl Joonas <114982893+karlidk@users.noreply.github.com>
2023-03-17 13:51:37 +0200

    Add files via upload

--- 05_kontrolltoo1/Cipher.java
+++ 05_kontrolltoo1/Cipher.java
@@ -1,79 +1,65 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Cipher {
-    private int shift;
+    private static final String INPUT_FILE = "input.txt";
+    private static final String OUTPUT_FILE = "output.txt";
 
-    public Cipher(int shift) {
-        this.shift = shift;
-    }
-
-    public String encrypt(String text) {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < text.length(); i++) {
-            char ch = text.charAt(i);
-
-            if (Character.isLetter(ch)) {
-                if (Character.isUpperCase(ch)) {
-                    char shiftedChar = (char) ('A' + (ch - 'A' + shift) % 26);
-                    result.append(shiftedChar);
-                } else {
-                    char shiftedChar = (char) ('a' + (ch - 'a' + shift) % 26);
-                    result.append(shiftedChar);
-                }
-            } else {
-                result.append(ch);
-            }
-        }
-
-        return result.toString();
-    }
-
-    public String decrypt(String text) {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < text.length(); i++) {
-            char ch = text.charAt(i);
-
-            if (Character.isLetter(ch)) {
-                if (Character.isUpperCase(ch)) {
-                    char shiftedChar = (char) ('Z' - ('Z' - ch + shift) % 26);
-                    result.append(shiftedChar);
-                } else {
-                    char shiftedChar = (char) ('z' - ('z' - ch + shift) % 26);
-                    result.append(shiftedChar);
-                }
-            } else {
-                result.append(ch);
-            }
-        }
-
-        return result.toString();
-    }
+    private static final int ALPHABET_SIZE = 26;
 
     public static void main(String[] args) {
-        File inputFile = new File("input.txt");
-        File outputFile = new File("output.txt");
+        // Loeme sisse sisendi failist
+        String input = readFromFile(INPUT_FILE);
 
-        try {
-            Scanner scanner = new Scanner(inputFile);
-            int shift = scanner.nextInt();
-            scanner.nextLine();
-            String text = scanner.nextLine();
-            Cipher cipher = new Cipher(shift);
-            String result = cipher.encrypt(text);
-            FileWriter writer = new FileWriter(outputFile);
-            writer.write(result);
-            writer.close();
-            System.out.println("Tekst on šifreeritud ja salvestatud faili output.txt");
+        // Teeme šifreerimise
+        String encrypted = encrypt(input, 1);
+
+        // Salvestame tulemuse väljundfaili
+        saveToFile(OUTPUT_FILE, encrypted);
+
+        // Dešifreerime šifreeritud sõnumi
+        String decrypted = decrypt(encrypted, 1);
+
+        // Väljastame dešifreeritud sõnumi
+        System.out.println("Decrypted message: " + decrypted);
+    }
+
+    public static String encrypt(String message, int shift) {
+        StringBuilder result = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                c = (char) (((c - base + shift) % ALPHABET_SIZE + ALPHABET_SIZE) % ALPHABET_SIZE + base);
+            }
+            result.append(c);
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String message, int shift) {
+        return encrypt(message, ALPHABET_SIZE - shift);
+    }
+
+    private static String readFromFile(String fileName) {
+        File file = new File("src/" + fileName);
+        StringBuilder result = new StringBuilder();
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                result.append(scanner.nextLine()).append("\n");
+            }
         } catch (FileNotFoundException e) {
-            System.out.println("Input faili ei leitud.");
-        } catch (IOException e) {
-            System.out.println("Error kirjutades faili.");
+            System.err.println("File not found: " + e.getMessage());
+        }
+        return result.toString().trim();
+    }
+
+    private static void saveToFile(String fileName, String content) {
+        try (PrintWriter writer = new PrintWriter("src/" + fileName)) {
+            writer.print(content);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
         }
     }
 }
